71c001d763b1fcd500b027ff2c71503553693a55
Carson Ip <carsonip715@gmail.com>
2022-03-25 18:28:13 +0000

    Add REPL

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def main():
-    pass
+import sys
+
+from monkey.lexer import Lexer
+from monkey.token import TokenType
+
+
+def prompt():
+    print('>> ', end='')
+    sys.stdout.flush()
+
+
+def repl():
+    prompt()
+    for line in sys.stdin:
+        lexer = Lexer(line)
+        while (tok := lexer.next_token()).type_ != TokenType.EOF:
+            print(tok)
+        prompt()
 
 
 if __name__ == '__main__':
-    main()
+    repl()
